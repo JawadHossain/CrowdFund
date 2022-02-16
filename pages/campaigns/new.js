667ba92da98f2e5948e94bf616df74e0e8374b9c
Jawad Hossain <jhossain@ualberta.ca>
2022-02-15 20:57:33 -0700

    Add header and new campaignpage

---
+++ pages/campaigns/new.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Layout from '../../components/Layout'
+
+class CampaignNew extends Component {
+    render() {
+        return <Layout>CampaignNew</Layout>
+    }
+}
+
+export default CampaignNew
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,41 @@
 import React, { Component } from 'react'
-import { Card } from 'semantic-ui-react'
+import { Card, Button } from 'semantic-ui-react'
+import Layout from '../components/Layout'
 import factory from '../ethereum/factory'
 
 class CampaignIndex extends Component {
-  static async getInitialProps () {
-    const campaigns = await factory.methods.getDeployedCampaigns().call()
+    static async getInitialProps() {
+        const campaigns = await factory.methods.getDeployedCampaigns().call()
 
-    return { campaigns }
+        return { campaigns }
+    }
 
-  }
+    renderCampaigns() {
+        const items = this.props.campaigns.map((address) => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true
+            }
+        })
 
-  renderCampaigns() {
-    const items = this.props.campaigns.map(address => {
-      return {
-        header: address,
-        description: <a>View Campaign</a>,
-        fluid: true
-      }
-    })
+        return <Card.Group items={items} />
+    }
 
-    return <Card.Group items={items} />
-  }
-
-  render() {
-    return <div>{this.renderCampaigns()}</div>
-  }
+    render() {
+        return (
+            <Layout>
+                <h3>Open Campaigns</h3>
+                <Button
+                    content="Create Campaign"
+                    icon="add circle"
+                    floated="right"
+                    primary
+                />
+                {this.renderCampaigns()}
+            </Layout>
+        )
+    }
 }
 
 export default CampaignIndex
